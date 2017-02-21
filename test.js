499fdd6188b99d1647c428452fdd0100e2563519
magicpuru <purushottam@spheremi.com>
2017-02-21 11:41:44 +0530

    test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 
 exports.handler = function(event, context) {
-  var res = v1.getLoad();
+  //var res = v1.getLoad();
   console.log(event.key1);
   context.succeed("hello");
 
