04448ae5bcd06b2583ec38546873ea799e3a673f
magicpuru <purushottam@spheremi.com>
2017-02-23 15:01:25 +0530

    test

--- api/load.js
+++ api/load.js
@@ -4,7 +4,7 @@ var load ={};
 
 load.options ={
 		method: 'GET',
-		uri: 'https://playground.avagree.com/api/carriers/1534531',
+		uri: 'https://playground.avagree.com/api/carriers/1550549',
 		headers: {
 		 'Authorization': '58a49df4c387a0755fb6ed97'
 	 }
